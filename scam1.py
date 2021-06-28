5538a46e5a92a54451fc8cad881e9c1b5888633a
SeniorPotato <kovacs_peter@yahoo.com>
2021-06-28 12:58:48 +0100

    Comments added and json file created from the passwordlist

--- scam1.py
+++ scam1.py
@@ -4,17 +4,20 @@ import random
 import string
 import json
 
-# You will need the URL, the location from 'network' tab where the email and password is being sent
+# Use this script to flood the scammer server with fake login credentials.
+# You will need the URL, from 'network' tab, in the dev chrome tool, where the email and password is being sent
 
 chars = string.ascii_letters + string.digits + '!£$%^&*()'
 random.seed = (os.urandom(1024))
 
+# Paste the url between the ''
 url = ''
 
 # Read in the json files
 names = json.loads(open('names.json').read())
 surnames = json.loads(open('surnames.json').read())
 emails = json.loads(open('email.json').read())
+password_list = json.loads(open('password_list.json').read())
 
 # join names and surnames (Where to add the '.' to split the surname and the name
 for name in names:
@@ -22,10 +25,11 @@ for name in names:
 
     # Generate random emails and passwords
     username = name.lower() + surnames.lower() + random.choice(emails)
-    password = ''.join(random.choice(chars) for i in range(8))
+    password = random.choice(password_list)
 
     # Get the location where the data is sent from the Chrome network tab
     requests.post(url, allow_redirects=False, data={
+        # paste the location of the login credentials here
         '': username,
         '': password
     })
